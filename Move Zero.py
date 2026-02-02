ff76457b500c1e604f24b76fc9f32a99f0f04211
ROHANVIJ2006 <rohan.vij2006@email.com>
2026-02-02 16:05:50 +0530

    Commited

--- Move Zero.py
+++ Move Zero.py
@@ -1,7 +1,15 @@
-nums=[0,1,0,3,12]
+'''nums=[0,1,0,3,12]
 j = 0  # index for non-zero placement
 for i in range(len(nums)):
     if nums[i] != 0:
         nums[i], nums[j] = nums[j], nums[i]
         j += 1
-print(nums)
+print(nums)'''
+r=range(10)
+r=iter(r)
+while True:
+    try:
+        d=next(r)
+        print(d)
+    except StopIteration:
+        break
