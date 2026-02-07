622cf6bf33c22f8ade4fdf073cdcb9014ff5ef8b
ROHANVIJ2006 <rohan.vij2006@email.com>
2026-02-07 15:12:06 +0530

    Commited

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def productExceptSelf(self, nums):
         n = len(nums)
         ans = [1] * n
@@ -13,4 +13,23 @@ class Solution:
             ans[i] *= right
             right *= nums[i]
 
-        return ans
+        return ans'''
+
+nums = [1, 2, 3, 4]
+ans = [1, 1, 1, 1]
+su = 1
+
+# First pass: calculate prefix products (left to right)
+for i in range(len(nums)):
+    ans[i] = su
+    su *= nums[i]
+
+# Reset su for suffix products
+su = 1
+
+# Second pass: multiply with suffix products (right to left)
+for j in range(len(nums)-1, -1, -1):
+    ans[j] *= su
+    su *= nums[j]
+
+print(ans)  # Output: [24, 12, 8, 6]
