1aed6f2cafb71e12c5b52d56e61f454c650a867d
ROHANVIJ2006 <rohan.vij2006@email.com>
2026-02-16 14:52:47 +0530

    Commited

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,42 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        H = None
+        P = None
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp = list1
+                list1 = list1.next
+            else:
+                temp = list2
+                list2 = list2.next
+
+            temp.next = None
+
+            if H == None:
+                H = P = temp
+            else:
+                P.next = temp
+                P = temp
+
+        while list1:
+            temp = list1
+            list1 = list1.next
+            temp.next = None
+            if H == None:
+                H = P = temp
+            else:
+                P.next = temp
+                P = temp
+
+        while list2:
+            temp = list2
+            list2 = list2.next
+            temp.next = None
+            if H == None:
+                H = P = temp
+            else:
+                P.next = temp
+                P = temp
+
+        return H
---
+++ Test.py
@@ -0,0 +1,20 @@
+
+'''def sum():
+    nums=[1,2,4,6]
+    index=0
+    m={}
+    target=5
+    for i in nums:
+        rss=target-i
+        print(rss)
+        if rss in m:
+            print("yes")
+            return [m[rss],index]
+        print(m)
+        m[i]=index
+        index+=1
+    return []
+a=sum()
+print(a)'''
+
+
