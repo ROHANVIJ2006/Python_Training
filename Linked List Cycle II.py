7c87739658561e8f1a826e13a86f0ab2c0b78a44
ROHANVIJ2006 <rohan.vij2006@email.com>
2026-02-23 15:55:15 +0530

    Commited

---
+++ Linked List Cycle II.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        slow = head
+        fast = head
+
+        # Step 1: Check if cycle exists
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            # Cycle detected
+            if slow == fast:
+
+                # Step 2: Find cycle start
+                slow = head
+
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+
+                return slow
+
+        # No cycle
+        return None
